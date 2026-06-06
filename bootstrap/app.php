1a209fe4cfe7a044cfedbd90cc67a9fd6133c594
Hoang Phuc <hoangphuc11tt@gmail.com>
2026-06-07 00:22:54 +0700

    Fix VNPAY URL generation on Render by trusting proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'ung-ho/vnpay-ipn',
         ]);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
