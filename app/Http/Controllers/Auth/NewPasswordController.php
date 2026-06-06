fc02c806451de94828c814214e07775acd29cd3e
Hoang Phuc <hoangphuc11tt@gmail.com>
2026-06-07 01:17:14 +0700

    Fix password column name to Mat_khau_hash in reset password controller

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -50,7 +50,7 @@ class NewPasswordController extends Controller
             $request->only('email', 'password', 'password_confirmation', 'token'),
             function (User $user) use ($request) {
                 $user->forceFill([
-                    'password' => Hash::make($request->password),
+                    'Mat_khau_hash' => Hash::make($request->password),
                     'remember_token' => Str::random(60),
                 ])->save();
 
